de269677a0b71d6847e2a25edd370ffd72219183
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2019-12-02 21:33:13 +0530

    Solves part one

--- src/2019/day2.go
+++ src/2019/day2.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -26,9 +27,61 @@ func day2() {
 		return
 	}
 
-	trimmedLine := strings.TrimSpace(line)
+	input := getIntCode(line)
+	fmt.Printf("Input IntCode :%v", input)
 
-	fmt.Println("********** Day 2 **********")
-	fmt.Printf("Trimmed Line :%v \n", trimmedLine)
-	fmt.Println("********** End Day 2 **********")
+	output := getCalculatedIntCode(input)
+	fmt.Printf("\n\nOutput IntCode :%v", output)
+}
+
+func getIntCode(inputLine string) []int {
+	trimmedLine := strings.TrimSpace(inputLine)
+	inputs := strings.Split(trimmedLine, ",")
+	inputNumber := []int{}
+
+	for _, input := range inputs {
+		converted, err := strconv.Atoi(input)
+
+		if err != nil {
+			log.Fatalf("\n\nError converting input : %v", err)
+			continue
+		}
+
+		inputNumber = append(inputNumber, converted)
+	}
+
+	return inputNumber
+}
+
+func getCalculatedIntCode(intCode []int) []int {
+	position := 0
+	length := len(intCode)
+
+	for position <= length {
+		opCode := intCode[position]
+
+		if opCode == 99 {
+			fmt.Print("\n\nOpcode 99 encountered. Aborting")
+			return intCode
+		}
+
+		if opCode == 1 || opCode == 2 {
+			firstPosition := intCode[position+1]
+			secondPosition := intCode[position+2]
+			resultPosition := intCode[position+3]
+
+			if opCode == 1 {
+				intCode[resultPosition] = intCode[firstPosition] + intCode[secondPosition]
+			} else {
+				intCode[resultPosition] = intCode[firstPosition] * intCode[secondPosition]
+			}
+		} else {
+			log.Fatalf("\n\nInvalid OpCode :%v", opCode)
+			break
+		}
+
+		position += 4
+	}
+
+	return intCode
 }
