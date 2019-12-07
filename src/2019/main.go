a8da9766ddeaf27bd9ea000060e5892dd36b4eb2
Ulhas Mandrawadkar <ulhas@users.noreply.github.com>
2019-12-07 23:49:16 +0530

    Completes day4 (#4)

--- src/2019/main.go
+++ src/2019/main.go
@@ -8,9 +8,10 @@ import (
 func main() {
 	start := time.Now()
 
-	day1()
-	day2()
-	day3()
+	// day1()
+	// day2()
+	// day3()
+	day4()
 
 	elapsed := time.Since(start)
 	log.Printf("--- Time taken %s ---", elapsed)
